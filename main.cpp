b3e4c85413a6bb7f454c85ca2324f386b9c6a057
Sachin Singh <sachinsingh.sk13@gmail.com>
2025-12-26 20:02:08 +0530

    fixed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 #include "graphs/graph_problems.h"
 using namespace std;
 
-int main()
-{
+int main() {
+	graph g = build_graph(5);
+	add_edge(g, 0, 1);
+	add_edge(g, 0, 2);
+	add_edge(g, 0, 3);
+	add_edge(g, 1, 2);
+	add_edge(g, 3, 4);
+	auto cg = complement(g);
+	print_graph(cg);
 	return 0;
 }
